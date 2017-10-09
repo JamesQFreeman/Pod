b7e221aa91f5010dcf200e9c3afe287fd6551db8
JamesQFreeman <ws.bond1@gmail.com>
2017-10-10 00:55:04 +0800

    Improve the broken English

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'podict',
+  packages = ['podict'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A command-line version Oxford Dictionary',
+  author = 'Sheng Wang',
+  author_email = 'wang_s1998@163.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Dictionary'], # arbitrary keywords
+  classifiers = [],
+)
